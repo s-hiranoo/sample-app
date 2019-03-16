93ab440d1a565b510fe0dfdcba9bd975af725fb0
s-hiranoo <hirano.pwork@gmail.com>
2019-03-16 16:28:53 +0900

    Create and test full_title helper

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+    
   test "should get root" do
     get root_url
     assert_response :success
@@ -13,8 +14,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | #{@base_title}"
-    
+    #assert_select "title", "Home | #{@base_title}"
+    assert_select "title", "#{@base_title}"
   end
 
   test "should get help" do
